259ae9fefb4ce1cc51af82ba7f098e9d72208e0b
Mahmood Ahmad <mahmood073@gmail.com>
2021-09-01 18:15:55 +0300

    restructring

---
+++ src/helpers.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!function_exists('dummy_function')) {
+    /**
+     * get the dummy function.
+     *
+     * @param  string $dummy
+     * @return string
+     */
+    function dummy_function($dummy = '')
+    {
+        return $dummy;
+    }
+}
