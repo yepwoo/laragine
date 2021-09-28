35cd1e55020d35dbd6962bfa7eebe5c02566ca35
Mahmood Ahmad <mahmood073@gmail.com>
2021-09-28 20:55:36 +0300

    Base and Unit views

---
+++ src/Core/Base/unit_template/edit.blade.php
@@ -0,0 +1,30 @@
+@extends($global['base']['namespace'] . 'layouts.master')
+
+@section('content')
+    <div class="section-header">
+        <h1>
+            Edit Unit
+            <a class="btn btn-success" href="{{ route($global['module']['routes']['index']) }}">
+                Go To All Units
+            </a>
+        </h1>
+    </div>
+
+    <div class="section-body">
+        <h2 class="section-title">Edit Unit</h2>
+        <p class="section-lead">edit the unit</p>
+
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>The Form</h4>
+                    </div>
+                    <div class="card-body">
+                        @include($global['module']['path'] .'._form')
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
