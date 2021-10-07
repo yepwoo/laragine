3df7b0f4e9afb89a38f8ce651b841853e2ac7df3
Mahmood Ahmad <mahmood073@gmail.com>
2021-10-07 17:24:38 +0200

    init String Manipulator

---
+++ src/Logic/StringManipulator.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Yepwoo\Laragine\Logic;
+
+use Illuminate\Support\Str;
+
+class StringManipulator
+{
+    /**
+     * get all possible shapes of a string
+     * 
+     * @param  string $string
+     * @return string[]
+     */
+    static public function generate($string)
+    {
+        $studly   = Str::studly($string);
+        $singular = Str::singular($studly);
+        $plural   = Str::plural($studly);
+
+        return [
+            'studly'              => $studly,
+            'singular'            => $singular,
+            'plural'              => $plural,
+            'singular_lower_case' => Str::lower($singular),
+            'plural_lower_case'   => Str::lower($plural)
+        ];
+    }
+}
