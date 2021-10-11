9a3414c33b1e37fc5770f6ad60dd3fe10168921a
Mahmood Ahmad <mahmood073@gmail.com>
2021-10-11 13:52:42 +0200

    no space

--- src/Generators/Payloads/Commands/Base.php
+++ src/Generators/Payloads/Commands/Base.php
@@ -5,7 +5,7 @@ namespace Yepwoo\Laragine\Generators\Payloads\Commands;
 use Yepwoo\Laragine\Generators\Payloads\GeneratorInterface;
 use Illuminate\Console\Command;
 
-class Base implements GeneratorInterface 
+class Base implements GeneratorInterface
 {
     /**
      * related command
