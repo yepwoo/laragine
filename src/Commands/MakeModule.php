86e7995606bce46417b2a0013871bd067e2b978e
Mahmood Ahmad <mahmood073@gmail.com>
2021-10-11 13:24:35 +0200

    update

--- src/Commands/MakeModule.php
+++ src/Commands/MakeModule.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Yepwoo\Laragine\Commands;
+
 use Illuminate\Console\Command;
 use Yepwoo\Laragine\Generators\Payloads\GeneratorFactory;
 
@@ -28,40 +30,9 @@ class MakeModule extends Command {
         parent::__construct();
     }
 
-
-    public function handle() {
-        $name = $this->argument('name');
-
-        $validatorFactory = \Yepwoo\Laragine\Logic\Validators\ValidatorFactory::create('module', $name);
-        $callback = $validatorFactory->valid();
-        
-        switch ($callback['flag']) {
-            case 'error':
-                $this->{$callback['flag']}($callback['msg']);
-                break;
-            case 'info' :
-                $module = GeneratorFactory::create('MakeModule', $name);
-                $this->{$callback['flag']}($callback['msg']);
-                break;
-            case 'confirm':
-                if ($this->confirm($callback['msg'], true)) {
-                    $module = GeneratorFactory::create('MakeModule', $name);
-                    $this->info("Done...");
-                }
-                break;
-        }
-        exit;
-        $module = GeneratorFactory::create('MakeModule', $name);
-        switch ($module->callback) {
-            case 'done':
-                $this->info('Module created');
-                break;
-            case 'created before':
-                $this->error('Module created before');
-                break;
-            default:
-                $this->error("Error... please try again");
-        }
+    public function handle()
+    {
+        $command = GeneratorFactory::create($this, 'MakeModule', $this->argument('name'));
+        $command->run();
     }
-
 }
--- src/Generators/Payloads/GeneratorFactory.php
+++ src/Generators/Payloads/GeneratorFactory.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Yepwoo\Laragine\Generators\Payloads;
 
 use Illuminate\Console\Command;
