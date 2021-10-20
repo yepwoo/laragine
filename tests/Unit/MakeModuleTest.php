0421d9e072ac442803f8f8c1f7643b83220fad9d
Mahmood Ahmad <mahmood073@gmail.com>
2021-10-20 16:47:47 +0200

    setting the tests correctly

--- tests/Unit/MakeModuleTest.php
+++ tests/Unit/MakeModuleTest.php
@@ -2,9 +2,7 @@
 
 namespace Yepwoo\Laragine\Tests\Unit;
 
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
-use phpDocumentor\Reflection\Types\Parent_;
 use Yepwoo\Laragine\Tests\TestCase;
 
 class MakeModuleTest extends TestCase
@@ -40,72 +38,33 @@ class MakeModuleTest extends TestCase
         $this->root_dir   = config('laragine.root_dir');
         $this->module_dir = $this->root_dir. '/' . $this->module;
     }
-
+    
     public function test_the_module_command_create_module_directory()
     {
-        // assume that we run install command to enure that core directory is exists
-        if(!File::isDirectory($this->root_dir)) {
-            Artisan::call('laragine:install');
-        }
-
-        // make sure we're starting from a clean state
-        if(File::exists($this->module_dir)) {
-            File::deleteDirectory($this->module_dir);
-        }
-
-        Artisan::call("laragine:module $this->module");
+        $this->artisan("laragine:module $this->module");
         $this->assertTrue(File::exists($this->module_dir));
     }
 
     public function test_when_module_directory_exists_users_can_choose_to_override_it()
     {
-        // assume that we run install command to enure that core directory is exists
-        if(!File::isDirectory($this->root_dir)) {
-            Artisan::call('laragine:install');
-        }
-
-        if(!File::isDirectory($this->module_dir)) {
-            Artisan::call("laragine:module $this->module");
-        }
         $command = $this->artisan("laragine:module $this->module");
 
         $command->expectsConfirmation(
-            'The root directory already exists, do you want to override it?', 'yes'
+            'the module directory already exists, do you want to override it?', 'yes'
         );
 
         $command->expectsOutput('Module created successfully!');
 
     }
-//
+
     public function test_when_module_directory_exists_users_can_choose_to_not_override_it()
     {
-        // assume that we run install command to enure that core directory is exists
-        if(!File::isDirectory($this->root_dir)) {
-            Artisan::call('laragine:install');
-        }
-
-        if(!File::exists($this->module_dir)) {
-            Artisan::call("laragine:module $this->module");
-        }
-
         $command = $this->artisan("laragine:module $this->module");
 
         $command->expectsConfirmation(
-            'The root directory already exists, do you want to override it?', 'no'
+            'the module directory already exists, do you want to override it?', 'no'
         );
 
         $command->expectsOutput('Existing module directory was not overwritten');
-    }
-    /**
-     * Clean up the testing environment before the next test.
-     *
-     * @return void
-     */
-    protected function tearDown(): void
-    {
-        Parent::tearDown();
-
-        // clean up
-        File::deleteDirectory($this->root_dir);
     }
 }
