5adf278ff6889aa4e025d0166d85b3cea2f73498
Mahmood Ahmad <mahmood073@gmail.com>
2021-10-24 17:42:35 +0200

    refactoring

--- src/Generators/Payloads/Commands/MakeUnit.php
+++ src/Generators/Payloads/Commands/MakeUnit.php
@@ -2,57 +2,11 @@
 
 namespace Yepwoo\Laragine\Generators\Payloads\Commands;
 
-use Illuminate\Console\Command;
 use Yepwoo\Laragine\Logic\StringManipulator;
 use Yepwoo\Laragine\Logic\Validators\ValidatorFactory;
 
 class MakeUnit extends Base
 {
-    /**
-     * module
-     *
-     * @var array
-     */
-    protected $module;
-
-    /**
-     * unit
-     *
-     * @var array
-     */
-    protected $unit;
-
-    /**
-     * init
-     *
-     * @var boolean
-     */
-    protected $init;
-
-    /**
-     * module collection
-     *
-     * @var array
-     */
-    protected $module_collection;
-
-    /**
-     * unit collection
-     *
-     * @var array
-     */
-    protected $unit_collection;
-
-    public function __construct(Command $command, $args)
-    {
-        parent::__construct($command, $args);
-        $this->module            = $this->args[0];
-        $this->unit              = $this->args[1];
-        $this->init              = $this->args[2];
-        $this->module_collection = StringManipulator::generate($this->module);
-        $this->unit_collection   = StringManipulator::generate($this->unit);
-    }
-
     /**
      * run the logic
      *
@@ -60,7 +14,33 @@ class MakeUnit extends Base
      */
     public function run()
     {
-        $validator = ValidatorFactory::create('Unit', $this->module, $this->unit, $this->command, $this->init);
+        $allow_publish     = true;
+        $unit_collection   = StringManipulator::generate($this->args[0]);
+        $module_collection = StringManipulator::generate($this->args[1]);
+        $init              = $this->args[2];
+
+        $validator = ValidatorFactory::create(
+            'Unit',
+            $module_collection['studly'],
+            $unit_collection['studly'],
+            $this->command,
+            $init
+        );
+
         $validator->valid();
+
+        if ($allow_publish) {
+            $this->publishUnit();
+        }
+    }
+
+    /**
+     * publish unit
+     * 
+     * @return void
+     */
+    protected function publishUnit()
+    {
+
     }
 }
