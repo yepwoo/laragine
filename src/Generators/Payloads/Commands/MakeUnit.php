a3d544b5249a37a654dcabbc8878d6d8c7c85878
Mahmood Ahmad <mahmood073@gmail.com>
2021-10-24 19:00:10 +0200

    adding more validations

--- src/Generators/Payloads/Commands/MakeUnit.php
+++ src/Generators/Payloads/Commands/MakeUnit.php
@@ -21,20 +21,42 @@ class MakeUnit extends Base
         $init              = $this->args[2];
         $module_dir        = $this->root_dir . '/' . $module_collection['studly'];
 
-        if(!FileManipulator::exists($module_dir)) {
+        if (!FileManipulator::exists($module_dir)) {
             $allow_publish = false;
             $this->command->error('Please create the module first');
         }
 
-        $validator = ValidatorFactory::create(
-            'Unit',
-            $module_collection['studly'],
-            $unit_collection['studly'],
-            $this->command,
-            $init
-        );
+        if ($init && FileManipulator::exists("{$module_dir}/data/{$unit_collection['studly']}.json")) {
+            $allow_publish = false;
+            $this->command->error('You already ran this command before');
+        }
 
-        $validator->valid();
+        if (!$init && !FileManipulator::exists("{$module_dir}/data/{$unit_collection['studly']}.json")) {
+            $allow_publish = false;
+            $this->command->error('Please type --init at the end of the command');
+        }
+
+        /**
+         * @todo check migrations, factories, requests, resources and tests as it's currently check for requests
+         */
+        if (!$init && FileManipulator::exists("{$module_dir}/Requests/{$unit_collection['studly']}Request.php")) {
+            if ($this->command->confirm('the unit already exists, do you want to override it?', true)) {
+                $allow_publish = true;
+            } else {
+                $allow_publish = false;
+                $this->command->warn('Existing unit was not overwritten');
+            }
+        }
+
+        // $validator = ValidatorFactory::create(
+        //     'Unit',
+        //     $module_collection['studly'],
+        //     $unit_collection['studly'],
+        //     $this->command,
+        //     $init
+        // );
+
+        // $validator->valid();
 
         if ($allow_publish) {
             $this->publishUnit();
