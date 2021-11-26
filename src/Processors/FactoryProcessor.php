5bd840d2884ec212884f775eef17cb13affb1b90
Abdlrahman <abdlrahmansaber2@gmail.com>
2021-11-26 15:04:43 +0200

    Json case in factory

--- src/Processors/FactoryProcessor.php
+++ src/Processors/FactoryProcessor.php
@@ -84,7 +84,11 @@ class FactoryProcessor extends Processor
             if(!$this->isEmpty($schema_types[$type]['factory']) && !$this->isArray($schema_types[$type]['factory'])) {
 
                 $type = $schema_types[$type]['factory'];
-                $this->type_str .= $type !== '' ? $type . '()': '';
+                if(strpos($type, 'json') !== false) {
+                    $this->type_str .= 'json_encode'.'(['."'$column_name'" .'=>' . "'test'". '])';
+                } else {
+                    $this->type_str .= $type !== '' ? $type . '()': '';
+                }
             }
             else if($this->isArray($schema_types[$type]['factory'])) { // have special cases
                 $special_cases = $schema_types[$type]['factory'];
