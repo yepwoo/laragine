c6e4b3f2d7bfebedc34642d626c6f9c6b18f1ca8
Abdlrahman <abdlrahmansaber2@gmail.com>
2021-12-08 09:00:41 +0200

    Add date to migration files

--- src/Processors/Factory.php
+++ src/Processors/Factory.php
@@ -51,7 +51,7 @@ class Factory
         $unit_plural_lower = $units_data['unit_collection']['plural_lower_case'];
 
         $replace = [
-            'file'    => ['php', $units_data['unit_collection']['studly'], $unit_plural_lower],
+            'file'    => ['php', $units_data['unit_collection']['studly'], date('Y_m_d_His'). '_' .$unit_plural_lower],
             'content' => [
                 $units_data['unit_collection']['studly'],
                 $unit_plural_lower,
