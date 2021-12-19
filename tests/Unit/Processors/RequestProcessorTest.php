f628ccc3b946cd18245351e190c9f212a0046411
Abdlrahman <abdlrahmansaber2@gmail.com>
2021-12-19 19:29:08 +0200

    Schema update for request - Request processor test

---
+++ tests/Unit/Processors/RequestProcessorTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Yepwoo\Laragine\Tests\Unit\Processors;
+
+use Yepwoo\Laragine\Processors\RequestProcessor;
+
+class RequestProcessorTest extends ProcessorTestCase
+{
+    public function test_output_str()
+    {
+        $processor    = new RequestProcessor($this->module_dir, $this->module_collection, $this->unit_collection);
+        $output_str   = $processor->process();
+        $expected_str = <<<STR
+                                            'name' => 'string|required',
+                                            'email' => 'string|unique:units|nullable',
+                                            'type' => 'string|unique:units|nullable',
+                                            'image_url' => 'string|unique:units|nullable',
+                                            'phone' => 'string|nullable',
+                        STR;
+        $expected_str = preg_replace("/\r/", "", $expected_str);
+        $this->assertEquals($expected_str, $output_str);
+    }
+}
